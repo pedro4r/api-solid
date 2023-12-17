74adec4de0a50b6da1f9a13550bc46298f09f9e0
Pedro Requiao <alonsofts@gmail.com>
2023-12-17 17:39:19 -0500

    add: Dependency Inversion Principle for prisma - creating findByEmail method

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -9,22 +9,16 @@ interface RegisterUseCaseRequest {
 
 export class RegisterUseCase {
     // eslint-disable-next-line prettier/prettier
-    constructor(private usersRepository: any) { }
+    constructor(private usersRepository: UsersRepository) { }
 
     async execute({ name, email, password }: RegisterUseCaseRequest) {
         const password_hash = await hash(password, 6)
 
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email,
-            },
-        })
+        const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
         if (userWithSameEmail) {
             throw new Error('E-mail already exists')
         }
-
-        // const prismaUsersRepository = new PrismaUsersRepository()
 
         await this.usersRepository.create({
             name,
