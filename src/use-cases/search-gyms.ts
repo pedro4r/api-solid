eb22ea21eac5aec8a10337fb4b003e14fa6128f7
Pedro Requiao <alonsofts@gmail.com>
2023-12-21 01:44:46 -0500

    add: create get gyms by name use case and test

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,25 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gym-repository'
+
+interface SearchGymsUseCaseRequest {
+    query: string
+    page: number
+}
+
+interface SearchGymsUseCaseResponse {
+    gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+    // eslint-disable-next-line prettier/prettier
+    constructor(private gymsRepository: GymsRepository) { }
+
+    async execute({
+        query,
+        page,
+    }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+        const gyms = await this.gymsRepository.searchMany(query, page)
+
+        return { gyms }
+    }
+}
