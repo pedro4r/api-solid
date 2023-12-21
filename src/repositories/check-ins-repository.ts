8adbd9d3c34fcbc5026b1e77ba018fb33aaac31e
Pedro Requiao <alonsofts@gmail.com>
2023-12-21 02:52:14 -0500

    add: create validate check-in use case and tests

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,7 @@ export interface CheckInsRepository {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+    findById(id: string): Promise<CheckIn | null>
     countByUserId(userId: string): Promise<number>
+    save(checkIn: CheckIn): Promise<Checkin>
 }
