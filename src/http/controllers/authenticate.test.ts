e16863dc7f2c8b77cc3d07605239339685ba9012
Pedro Requiao <alonsofts@gmail.com>
2023-12-22 23:56:05 -0500

    add: test e2e to authenticate and profile

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -1,5 +1,29 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-// eslint-disable-next-line prettier/prettier
-test('ok', () => { })
+describe('Authenticate (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    afterAll(async () => {
+        await app.close()
+    })
+    it('should be able to authenticate', async () => {
+        await request(app.server).post('/users').send({
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password: '123456',
+        })
+
+        const response = await request(app.server).post('/sessions').send({
+            email: 'john.doe@example.com',
+            password: '123456',
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String),
+        })
+    })
+})
